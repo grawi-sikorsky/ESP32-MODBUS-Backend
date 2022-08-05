2aa6c1608ded4e6b8bf9687e0c193d6e5fedf053
JobLaptok <grawires@gmail.com>
2022-08-05 18:16:44 +0200

    init commit

---
+++ src/main/java/pl/js/modbus/ModbusApplication.java
@@ -0,0 +1,13 @@
+package pl.js.modbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/js/modbus/ModbusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.js.modbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

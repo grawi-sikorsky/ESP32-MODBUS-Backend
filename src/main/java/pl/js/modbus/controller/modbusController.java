4cadba784df4f1a5002d0e270409fcd5bf4a7a21
JobLaptok <grawires@gmail.com>
2022-08-07 19:28:15 +0200

    tst

--- src/main/java/pl/js/modbus/controller/modbusController.java
+++ src/main/java/pl/js/modbus/controller/modbusController.java
@@ -1,6 +1,7 @@
 package pl.js.modbus.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,12 @@ public class modbusController {
     @GetMapping("/a")
     public String gecior(){
         return "aaa";
+    }
+
+    @PostMapping("/data")
+    public String sendData(){
+
+        return "sending data...";
     }
     
 }

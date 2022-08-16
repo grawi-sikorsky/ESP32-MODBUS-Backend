2088dc501db02d2007a50c927d3c51ecb46e221f
JobLaptok <grawires@gmail.com>
2022-08-16 12:37:55 +0200

    Update ModbusController.java

--- src/main/java/pl/js/modbus/controller/ModbusController.java
+++ src/main/java/pl/js/modbus/controller/ModbusController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.js.modbus.model.DataModel;
@@ -12,6 +13,7 @@ import pl.js.modbus.service.DataService;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
+@RequestMapping("/data")
 public class ModbusController {
 
     private final DataService dataService;
@@ -20,12 +22,12 @@ public class ModbusController {
         this.dataService = dataService;
     }
 
-    @GetMapping("/data")
+    @GetMapping
     public List<DataModel> showData(){
         return dataService.getData();
     }
 
-    @PostMapping("/data")
+    @PostMapping
     public DataModel receiveData(@RequestBody DataModel incomingDataModel){
 
         return dataService.saveData(incomingDataModel);

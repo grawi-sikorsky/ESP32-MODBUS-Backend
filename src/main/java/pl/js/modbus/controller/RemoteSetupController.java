3a618cbacf5f39b55f26e3ad3fac9a1e24204bac
JobLaptok <grawires@gmail.com>
2022-08-18 16:43:39 +0200

    cors

--- src/main/java/pl/js/modbus/controller/RemoteSetupController.java
+++ src/main/java/pl/js/modbus/controller/RemoteSetupController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.js.modbus.model.SetupModel;
 import pl.js.modbus.service.RemoteSetupService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://modbuspanel.herokuapp.com:8080"})
 @RestController
 @RequestMapping("/setup")
 public class RemoteSetupController {

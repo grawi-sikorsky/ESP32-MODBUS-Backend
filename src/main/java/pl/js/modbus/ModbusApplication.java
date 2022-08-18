2040016930f381d703276a0db59a15cbf2611a7b
JobLaptok <grawires@gmail.com>
2022-08-18 16:51:32 +0200

    Update ModbusApplication.java

--- src/main/java/pl/js/modbus/ModbusApplication.java
+++ src/main/java/pl/js/modbus/ModbusApplication.java
@@ -1,7 +1,13 @@
 package pl.js.modbus;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 // @EnableAutoConfiguration
 @SpringBootApplication
@@ -11,4 +17,21 @@ public class ModbusApplication {
 		SpringApplication.run(ModbusApplication.class, args);
 	}
 
+	// CORS CONFIG
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(
+            Arrays.asList("GET","POST","HEAD","DELETE","PUT","OPTIONS"));
+        configuration.setMaxAge(1l);
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+	}
 }
+
+
+
